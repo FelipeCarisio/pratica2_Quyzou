5baecbe32ff9519968b4db9a4b2841212e81f1ef
FelipeCarisio <44804658+FelipeCarisio@users.noreply.github.com>
2019-05-22 11:41:02 -0300

    a

---
+++ pratica2/Controllers/JogarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pratica2.Controllers
+{
+    public class JogarController : Controller
+    {
+        // GET: Jogar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ pratica2/Controllers/RankingsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pratica2.Controllers
+{
+    public class RankingsController : Controller
+    {
+        // GET: Rankings
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
