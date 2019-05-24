8de9de5111f6dcca26559c130a056cae9c77a7a6
FelipeCarisio <44804658+FelipeCarisio@users.noreply.github.com>
2019-05-24 11:39:57 -0300

    a

---
+++ pratica2/DAO/quizDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using pratica2.Models;
+
+namespace pratica2.DAO
+{
+    public class quizDAO
+    {
+    }
+}
