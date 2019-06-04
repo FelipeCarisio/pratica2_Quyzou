8aeb408fbb4eb830d2922eb09de77ec97cd352be
FelipeCarisio <44804658+FelipeCarisio@users.noreply.github.com>
2019-06-04 11:59:47 -0300

    a

--- pratica2/Controllers/JogarController.cs
+++ pratica2/Controllers/JogarController.cs
@@ -5,18 +5,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace pratica2.Controllers
 {
     public class JogarController : Controller
     {
         // GET: Jogar
+
+        [Route("Jogar")]
+
         public ActionResult Index()
         {
             ViewBag.Title = "Jogar";
             return View();
         }
 
+        [Route("Jogar/{pontos}")]
         public ActionResult Pontuar(int pontos)
         {              
             usuario usu = new usuario();
