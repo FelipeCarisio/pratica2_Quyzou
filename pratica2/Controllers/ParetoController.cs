ee3b2067ceb30252eeb545b7ea1f617a5adcdba3
FelipeCarisio <44804658+FelipeCarisio@users.noreply.github.com>
2019-06-06 09:52:23 -0300

    Uu

---
+++ pratica2/Controllers/ParetoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pratica2.Controllers
+{
+    public class ParetoController : Controller
+    {
+        // GET: Pareto
+        public ActionResult Index()
+        {
+            ViewBag.Titile = "Pareto";
+            return View();
+        }
+    }
+}
